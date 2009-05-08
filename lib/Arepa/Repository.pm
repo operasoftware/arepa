10700693103241ef9c5134b73d4b540cb7c6bfe6
Esteban Manchado Velázquez <estebanm@opera.com>
2009-05-08 17:55:35 +0000

    * Implement the first part of the reprepro integration: when a source package   is added with insert_source_package, add it to the repo with reprepro * This is actually related to the test in the previous commit: it should fail   without this changeset

--- lib/Arepa/Repository.pm
+++ lib/Arepa/Repository.pm
@@ -5,6 +5,8 @@ use warnings;
 
 use Carp qw(croak);
 
+use lib qw(../Parse-Debian-Changes/trunk/lib/);
+use Parse::Debian::PackageDesc;
 use Arepa::Config;
 use Arepa::PackageDb;
 
@@ -62,8 +64,64 @@ sub get_repository_architectures {
 }
 
 sub insert_source_package {
-    my ($self, @args) = @_;
-    $self->{package_db}->insert_source_package(@args);
+    my ($self, $dsc_file, $distro) = @_;
+
+    use Parse::Debian::PackageDesc;
+    my $parsed_dsc = Parse::Debian::PackageDesc->new($dsc_file);
+    my %args = (name         => $parsed_dsc->name,
+                full_version => $parsed_dsc->version,
+                architecture => $parsed_dsc->architecture,
+                distribution => $distro);
+
+    my $r = $self->_execute_reprepro('includedsc',
+                                     $args{distribution},
+                                     $dsc_file);
+    if ($r) {
+        return $self->{package_db}->insert_source_package(%args);
+    }
+    else {
+        return 0;
+    }
+}
+
+sub _shell_escape {
+    my ($self, $arg) = @_;
+    $arg =~ s/'/\\'/go;
+    return "'$arg'";
+}
+
+sub last_cmd_output {
+    my ($self) = @_;
+    $self->{last_cmd_output};
+}
+
+sub _execute_reprepro {
+    my ($self, $mode, $distro, $file_path, %extra_args) = @_;
+
+    my $repo_path = $self->get_config_key("repository:path");
+    $mode      = $self->_shell_escape($mode);
+    $distro    = $self->_shell_escape($distro);
+    $file_path = $self->_shell_escape($file_path);
+    # Extra arguments
+    my $extra = "";
+    foreach my $arg (keys %extra_args) {
+        # Section and priority options are actually flipped in reprepro (that's
+        # a reprepro bug)
+        if ($arg eq 'section') {
+            $extra .= " --priority " . $self->_shell_escape($extra_args{$arg});
+        }
+        elsif ($arg eq 'priority') {
+            $extra .= " --section " . $self->_shell_escape($extra_args{$arg})
+        }
+        else {
+            croak "Don't know anything about argument '$arg'";
+        }
+    }
+
+    my $cmd = "reprepro -b$repo_path $mode $distro $file_path $extra 2>&1";
+    $self->{last_cmd_output} = `$cmd`;
+    my $status = $?;
+    return ($status == 0);
 }
 
 1;
