32514671dafb727b9e06555bb28a34fc4e61463f
Esteban Manchado Velázquez <estebanm@opera.com>
2009-05-10 17:52:41 +0000

    * Add documentation for Arepa::Config

--- lib/Arepa/Config.pm
+++ lib/Arepa/Config.pm
@@ -64,3 +64,80 @@ sub get_builder_config_key {
 }
 
 1;
+
+__END__
+
+=head1 NAME
+
+Arepa::Config - Arepa package database API
+
+=head1 SYNOPSIS
+
+ my $config = Arepa::Config->new('path/to/config.yml');
+ my $pdb_path = $config->get_key('package_db');
+ my $repo_path = $config->get_key('repository:path');
+ my @builder_names = $config->get_builders;
+ my %builder_config = $config->get_builder_config('some-builder');
+ my $value = $config->get_builder_config_key('some-builder', $key);
+
+=head1 DESCRIPTION
+
+This class allows easy access to the Arepa configuration. The configuration is
+stored in a YAML file, and these are the structure for it:
+
+ ---
+ repository:
+   path: /home/zoso/src/apt-web/test-repo/
+ upload_queue:
+   path: /home/zoso/src/apt-web/incoming
+ package_db: /home/zoso/src/apt-web/package.db
+ web_ui:
+   base_url: http://localhost
+   template_dir: /home/zoso/src/apt-web/repo-tools-web/templates/
+   user_file: /home/zoso/src/apt-web/repo-tools-web/users.yml
+ builders:
+   - name: squeeze32
+     type: sbuild
+     architecture: i386
+     distribution: my-squeeze
+     other_distributions: [squeeze, unstable]
+   - name: squeeze64
+     type: sbuild
+     architecture: amd64
+     architecture_all: yes
+     distribution: my-squeeze
+     other_distributions: [squeeze, unstable]
+
+Usually this class is not used directly, but internally in C<Arepa::Repository>
+or C<Arepa::BuilderFarm>.
+
+=head1 METHODS
+
+=over 4
+
+=item new($path)
+
+It creates a new configuration access object for the configuration file in the
+given C<$path>.
+
+=item get_key($key)
+
+Returns the value of the given C<$key> in the configuration file. If it's not a
+top-level key, the subkeys must be separated by a colon ("C<:>"). If the key
+cannot be found, an exception is thrown.
+
+=item get_builders
+
+Returns an array of names of the defined builders.
+
+=item get_builder_config($builder_name)
+
+Returns a hash with the configuration of the given C<$builder_name>. If no
+builder (or more than one) is found by that name, an exception is thrown.
+
+=item get_builder_config_key($builder_name, $key)
+
+Returns the value for the given configuration C<$key> for the given
+C<$builder_name>.
+
+=back
