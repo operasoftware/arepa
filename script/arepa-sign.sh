70e1e1127b802bc62d23a436fd7581faf6fe89ee
Esteban Manchado Velázquez <estebanm@opera.com>
2010-03-15 12:48:47 +0000

    * Add /etc/sudoers instructions to the signing script

--- script/arepa-sign.sh
+++ script/arepa-sign.sh
@@ -2,5 +2,8 @@
 
 # Signs the repositories defined for Arepa. It just calls "arepa sign" as the
 # arepa-master user
+# You probably want to add a line like this to your /etc/sudoers file:
+# 
+#     %arepa ALL=(ALL) NOPASSWD: /usr/bin/arepa-sign
 
 su - arepa-master -c "/usr/bin/arepa sign"
