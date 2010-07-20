ccf01e952e9b8e84aa3f432a0cae5386bb8ff160
Esteban Manchado Velázquez <estebanm@opera.com>
2010-07-20 15:07:04 +0000

    Implement the "Log out" action

--- lib/Arepa/Web/Auth.pm
+++ lib/Arepa/Web/Auth.pm
@@ -3,15 +3,17 @@ package Arepa::Web::Auth;
 use strict;
 use warnings;
 
-use base 'Mojolicious::Controller';
+use base 'Arepa::Web::Base';
 use MojoX::Session;
 use DBI;
 use Digest::MD5;
 
+my $session_db = "sessions.db";
+
 sub login {
     my $self = shift;
 
-    my $dbh = DBI->connect("dbi:SQLite:dbname=sessions.db");
+    my $dbh = DBI->connect("dbi:SQLite:dbname=$session_db");
     my $session = MojoX::Session->new(tx    => $self->tx,
                                       store => [dbi => {dbh => $dbh}]);
 
@@ -41,6 +43,18 @@ sub login {
         return 1;
     }
     $self->render('auth/login');
+}
+
+sub logout {
+    my $self = shift;
+
+    my $dbh = DBI->connect("dbi:SQLite:dbname=$session_db");
+    my $session = MojoX::Session->new(tx    => $self->tx,
+                                      store => [dbi => {dbh => $dbh}]);
+    $session->expire;
+    $session->flush;
+
+    $self->redirect_to($self->config->get_key('web_ui:cgi_base_url'));
 }
 
 1;
