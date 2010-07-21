a7fe8fbc20e0ede3c969dfe62331e9bd170e135a
Esteban Manchado Velázquez <estebanm@opera.com>
2010-07-21 13:08:01 +0000

    Give the '/' route a name, and use it in the templates

--- lib/Arepa/Web.pm
+++ lib/Arepa/Web.pm
@@ -15,7 +15,7 @@ sub startup {
     my $auth = $r->bridge->to('auth#login');
 
     # Default route
-    $auth->route('/')->to('dashboard#index');
+    $auth->route('/')->to('dashboard#index')->name('home');
     $auth->route('/:controller/:action/:id')->name('generic_id');
     $auth->route('/:controller/:action')->name('generic');
     $auth->route('/:controller')->to(action => 'index')->name('onlycontroller');
