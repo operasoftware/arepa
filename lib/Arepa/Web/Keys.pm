61b7da42b9cee83d8b1c2051306a3bdb7f844df4
Esteban Manchado Velázquez <estebanm@opera.com>
2010-08-11 15:03:35 +0000

    Add a very simple GPG key import feature

--- lib/Arepa/Web/Keys.pm
+++ lib/Arepa/Web/Keys.pm
@@ -19,4 +19,22 @@ sub index {
                        output => $gpg_list_keys_output });
 }
 
+sub import {
+    my ($self) = @_;
+
+    my $gpg_homedir = $self->config->get_key('web_ui:gpg_homedir');
+    my $gpg_import_cmd = "gpg --homedir '$gpg_homedir' " .
+                            "--no-default-keyring --import";
+    my $r = open F, "| $gpg_import_cmd";
+    if ($r) {
+        print F $self->param("gpgkeys");
+        close F;
+
+        $self->redirect_to('generic', action => 'index');
+    }
+    else {
+        $self->show_view({ error => $! });
+    }
+}
+
 1;
