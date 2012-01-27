f0b7519703988854388c4cf8238e71a9a582b723
Esteban Manchado Velázquez <estebanm@opera.com>
2012-01-27 15:39:46 +0000

    Add RSS feed to get the latest packages

--- lib/Arepa/Web/Public.pm
+++ lib/Arepa/Web/Public.pm
@@ -11,6 +11,8 @@ use File::stat;
 use XML::RSS;
 use Parse::Debian::PackageDesc;
 
+use Arepa::PackageDb;
+
 sub _retarded_escape {
     my ($self, $value) = @_;
 
@@ -94,6 +96,72 @@ sub rss {
                             ".<br/>" . $signature_info,
             pubDate     => strftime("%a, %d %b %Y %H:%M:%S %z",
                                     localtime(stat($pkg->path)->mtime)),
+        );
+    }
+
+    $self->render_text($rss->as_string);
+}
+
+sub latest_packages_rss {
+    my ($self) = @_;
+
+    my $rss = XML::RSS->new(version => '2.0');
+    $rss->channel(
+        title        => "Arepa latest packages",
+        link         => "http://search.cpan.org/~opera/",
+        description  => "Latest packages added to the repository",
+        dc => {
+            date       => '2012-01-27T16:20+00:00',
+            subject    => "Software distribution",
+            creator    => 'estebanm@opera.com',
+            language   => 'en-us',
+        },
+        syn => {
+            updatePeriod     => "hourly",
+            updateBase       => "1901-01-01T00:00+00:00",
+        },
+        taxo => [
+            'http://dmoz.org/Computers/Software/Operating_Systems/Linux/Distributions/Debian/',
+        ]
+    );
+
+
+    my $packagedb =
+            Arepa::PackageDb->new($self->config->get_key('package_db'));
+    my @packages = ();
+    my @compilation_queue = $packagedb->
+                        get_compilation_queue(status => 'compiled',
+                                              order  => "compilation_completed_at DESC",
+                                              limit  => 30);
+    foreach my $comp (@compilation_queue) {
+        my %source_pkg_attrs =
+            $packagedb->get_source_package_by_id($comp->{source_package_id});
+        push @packages, {
+            %$comp,
+            package => { %source_pkg_attrs },
+        };
+    }
+
+    my $public_url;
+    if ($self->config->key_exists('web_ui:cgi_base_url')) {
+        $public_url = $self->config->get_key('web_ui:cgi_base_url');
+    }
+    if ($self->config->key_exists('web_ui:public_url')) {
+        $public_url = $self->config->get_key('web_ui:public_url');
+    }
+    if (!defined $public_url) {
+        die "Couldn't find configuration keys web_ui:public_url or web_ui:cgi_base_url";
+    }
+
+    foreach my $pkg (@packages) {
+        $rss->add_item(
+            title       => $pkg->{package}->{name} . " " . $pkg->{package}->{full_version} . " (" .
+                            $pkg->{distribution} . "/" . $pkg->{architecture} . ")",
+            link        => $public_url,
+            description => $pkg->{package}->{name} . " " . $pkg->{package}->{full_version} .
+                            "<br/>" .
+                            $pkg->{package}->{comments},
+            pubDate     => $pkg->{completed_at},
         );
     }
 
